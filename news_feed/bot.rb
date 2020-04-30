4cbd98f901e1f207b01fc6894334e443e13efbe4
kbjude <kbjude@gmail.com>
2020-04-30 20:07:36 +0300

    commands for getting a site added

--- news_feed/bot.rb
+++ news_feed/bot.rb
@@ -5,9 +5,27 @@ module NewsFeed
             desc 'The bot is about getting the trending news'
 
             command :get_most_trending_news do
-            title   'get_most_trending_news'
-            desc    'Returns the URL of the most trending news'
-            long_desc 'Returns the URL of the most trending news'
+                title   'get_most_trending_news'
+                desc    'Returns the URL of the most trending news'
+                long_desc 'Returns the URL of the most trending news'
+            end
+
+            command :second_most_trending_news do
+                title   'Second_most_trending_news'
+                desc    'Returns the URL that is trending in the second place'
+                long_desc 'Returns the most trending URL after the first'
+            end
+
+            command :third_most_trending_news do
+                title   'Third_most_trending_news'
+                desc    'Returns the URL that is trending in the third place'
+                long_desc 'Returns the most trending URL after the second'
+            end
+
+            command :fourth_most_trending_news do
+                title   'Fourth_most_trending_news'
+                desc    'Returns the URL that is trending in the fourth place'
+                long_desc 'Returns the most trending URL after the second'
             end
         end
     end
--- news_feed/commands/get_news.rb
+++ news_feed/commands/get_news.rb
@@ -4,9 +4,25 @@ require 'open-uri'
 module NewsFeed
   module Commands
     class GetNews < SlackRubyBot::Commands::Base
+      url = 'http://fetchrss.com/rss/5eaaedad8a93f885248b45685eaaed938a93f8ef238b4567.xml'
+      rss_value = RSS::Parser.parse(open(url).read, false).items
+
       command 'get_most_trending_news' do |client, data, _match|
-        url = 'http://fetchrss.com/rss/5eaaedad8a93f885248b45685eaaed938a93f8ef238b4567.xml'
-        rss = RSS::Parser.parse(open(url).read, false).items
+        rss = rss_value.first
+        client.say(channel: data.channel, text: rss.link)
+      end
+      command 'second_most_trending_news' do |client, data, _match|
+        rss = rss_value.second
+        client.say(channel: data.channel, text: rss.link)
+      end
+
+      command 'third_most_trending_news' do |client, data, _match|
+        rss = rss_value.third
+        client.say(channel: data.channel, text: rss.link)
+      end
+
+      command 'fourth_most_trending_news' do |client, data, _match|
+        rss = rss_value.fourth
         client.say(channel: data.channel, text: rss.link)
       end
       command 'say_hello' do |client, data, _match|
