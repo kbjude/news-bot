df828ce011ae7072e8ae5c5a8c39546259a86e39
kbjude <kbjude@gmail.com>
2020-04-30 18:25:28 +0300

    successful site

--- news_feed/commands/get_news.rb
+++ news_feed/commands/get_news.rb
@@ -5,11 +5,11 @@ module NewsFeed
   module Commands
     class GetNews < SlackRubyBot::Commands::Base
       command 'get_most_trending_news' do |client, data, _match|
-        url = 'https://news.ycombinator.com/news'
-        rss = RSS::Parser.parse(open(url).read, false).items.first
+        url = 'http://fetchrss.com/rss/5eaaedad8a93f885248b45685eaaed938a93f8ef238b4567.xml'
+        rss = RSS::Parser.parse(open(url).read, false).items
         client.say(channel: data.channel, text: rss.link)
       end
-      command 'say_hello' do |client, data, _matcg|
+      command 'say_hello' do |client, data, _match|
         client.say(channel: data.channel, text: Greeting.greet)
       end
     end
