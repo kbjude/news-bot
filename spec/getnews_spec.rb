355aff3f7d094e76754b89fcf07553c4ab325d83
kbjude <kbjude@gmail.com>
2020-05-01 13:14:49 +0300

    spec descriptions

--- spec/getnews_spec.rb
+++ spec/getnews_spec.rb
@@ -6,11 +6,11 @@ RSpec.describe NewsFeed::Commands::GetNews do
   let(:url) { 'https://en.wikipedia.org/wiki/The_Case_of_the_Speluncean_Explorers' }
 
   describe '#get_response' do
-    context 'Gets news feed' do
-      it 'when the top most feed is called foe' do
+    context 'Gets response' do
+      it ' indicating that wrong information is input' do
         expect(news.get_response(1_000_000)).to eql('not found')
       end
-      it 'gets most trending news' do
+      it 'returns the requested for link' do
         expect(news.get_response(0)).to eql(url)
       end
     end
