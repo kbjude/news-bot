470df1ceebfc59b93b184a0d2c57050ebe155c1c
kbjude <kbjude@gmail.com>
2020-05-01 10:29:34 +0300

    WIP get functions

--- news_feed/commands/get_news.rb
+++ news_feed/commands/get_news.rb
@@ -5,35 +5,39 @@ module NewsFeed
   module Commands
     # Scraping the nes from the site
     class GetNews < SlackRubyBot::Commands::Base
-      url = 'http://fetchrss.com/rss/5eaaedad8a93f885248b45685eaaed938a93f8ef238b4567.xml'
-      rss_value = RSS::Parser.parse(open(url).read, false).items
+
+      def self.get_response(index)
+        url = 'http://fetchrss.com/rss/5eaaedad8a93f885248b45685eaaed938a93f8ef238b4567.xml'
+        rss_value = RSS::Parser.parse(open(url).read, false).items
+        text = 'not found'
+        text = rss_value[index].link if rss_value.count > index
+        text
+      end
+
+      def self.hello
+        'Hello, this bot will give you the news!'
+      end
 
       command 'get_most_trending_news' do |client, data, _match|
-        rss = rss_value.first
-        client.say(channel: data.channel, text: rss.link)
+        text = get_response(0)
+        client.say(channel: data.channel, text: text)
       end
       command 'second_most_trending_news' do |client, data, _match|
-        rss = rss_value.second
-        client.say(channel: data.channel, text: rss.link)
+        text = get_response(1)
+        client.say(channel: data.channel, text: text)
       end
 
       command 'third_most_trending_news' do |client, data, _match|
-        rss = rss_value.third
-        client.say(channel: data.channel, text: rss.link)
+        text = get_response(2)
+        client.say(channel: data.channel, text: text)
       end
 
       command 'fourth_most_trending_news' do |client, data, _match|
-        rss = rss_value.fourth
-        client.say(channel: data.channel, text: rss.link)
+        text = get_response(3)
+        client.say(channel: data.channel, text: text)
       end
       command 'say_hello' do |client, data, _match|
-        client.say(channel: data.channel, text: Greeting.greet)
-      end
-    end
-    # A welcome remark
-    class Greeting
-      def self.greet
-        'Hello, this bot will give you the news!'
+        client.say(channel: data.channel, text: hello)
       end
     end
   end
