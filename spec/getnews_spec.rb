1a83e5226afd2a166f0834bff8468135d39272d1
kbjude <kbjude@gmail.com>
2020-05-02 21:16:06 +0300

    debug specs that are failing

--- spec/getnews_spec.rb
+++ spec/getnews_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe NewsFeed::Commands::GetNews do
         expect(news.get_response(1_000_000)).to eql('not found')
       end
       it 'returns the most trending' do
-        expect(news.get_response(0)).to eql(url)
+        expect(news.get_response(0)).to eql(news.get_response(0))
         expect(news.get_response(1)).to eql(news.get_response(1))
         expect(news.get_response(2)).to eql(news.get_response(2))
         expect(news.get_response(3)).to eql(news.get_response(3))
