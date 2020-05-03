eb515d6c3725352319395e954be1654122be8168
kbjude <kbjude@gmail.com>
2020-05-03 14:33:51 +0300

    Private methods approach changed

--- news_feed/commands/get_news.rb
+++ news_feed/commands/get_news.rb
@@ -3,7 +3,7 @@ require 'open-uri'
 
 module NewsFeed
   module Commands
-    # Scraping the nes from the site
+    # Scraping the news from the site
     class GetNews < SlackRubyBot::Commands::Base
       def self.get_response(index)
         url = 'http://fetchrss.com/rss/5eaaedad8a93f885248b45685eaaed938a93f8ef238b4567.xml'
@@ -18,25 +18,21 @@ module NewsFeed
       end
 
       command 'get_most_trending_news' do |client, data, _match|
-        text = get_response(0)
-        client.say(channel: data.channel, text: text)
+        client.say(channel: data.channel, text: GetNews.get_response(0))
       end
       command 'second_most_trending_news' do |client, data, _match|
-        text = get_response(1)
-        client.say(channel: data.channel, text: text)
+        client.say(channel: data.channel, text: GetNews.get_response(1))
       end
 
       command 'third_most_trending_news' do |client, data, _match|
-        text = get_response(2)
-        client.say(channel: data.channel, text: text)
+        client.say(channel: data.channel, text: GetNews.get_response(2))
       end
 
       command 'fourth_most_trending_news' do |client, data, _match|
-        text = get_response(3)
-        client.say(channel: data.channel, text: text)
+        client.say(channel: data.channel, text: GetNews.get_response(3))
       end
       command 'say_hello' do |client, data, _match|
-        client.say(channel: data.channel, text: hello)
+        client.say(channel: data.channel, text: GetNews.hello)
       end
     end
   end
